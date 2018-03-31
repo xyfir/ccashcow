89eaea27ce9fee6a36aecd324998a3794d47d8ff
MrXyfir <mr@xyfir.com>
2018-03-30 17:40:10 -0700

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
+const config = require('./server/constants/config');
+const path = require('path');
+
+const plugins = [];
+const isProd = config.ENVIRONMENT == 'production';
+
+if (isProd) {
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz'
+    })
+  );
+}
+
+module.exports = {
+
+  mode: config.ENVIRONMENT,
+
+  entry: {
+    Admin: './client/components/Admin.jsx',
+    App: './client/components/App.jsx'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'static/js')
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'client'),
+      'node_modules'
+    ],
+    alias: {
+      server: __dirname
+    },
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'client')
+      ],
+      exclude: /node_modules/,
+      options: {
+        presets: ['env', 'react']
+      }
+    }]
+  },
+
+  plugins
+
+};
