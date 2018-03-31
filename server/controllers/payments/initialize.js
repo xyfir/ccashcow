4bf3ebfb919bebd7b51faa590bfaf7314cf846ad
MrXyfir <mr@xyfir.com>
2018-03-30 20:31:50 -0700

    Add `POST /api/payments/initialize`

--- server/controllers/payments/initialize.js
+++ server/controllers/payments/initialize.js
@@ -1,3 +1,50 @@
+const authorizeSeller = require('lib/sellers/authorize');
+const MySQL = require('lib/MySQL');
+
+/**
+ * @typedef {object} RequestBody
+ * @prop {number} seller_id
+ * @prop {string} seller_key
+ * @prop {string[]} methods
+ * @prop {number} amount
+ * @prop {string} description
+ * @prop {object} info
+ */
+/**
+ * @typedef {object} ResponseBody
+ * @prop {string} [message]
+ * @prop {number} [id]
+ */
+/**
+ * `POST /api/payments/initialize`
+ * @param {object} req
+ * @param {RequestBody} req.body
+ * @param {object} res
+ */
 module.exports = async function(req, res) {
+
+  const db = new MySQL;
+
+  try {
+    await authorizeSeller(db, req.body.seller_id, req.body.seller_key);
+
+    const result = db.query(`
+      INSERT INTO payments SET ?
+    `, {
+      description: req.body.description,
+      seller_id: req.body.seller_id,
+      methods: JSON.stringify(req.body.methods),
+      method: req.body.method,
+      amount: req.body.amount,
+      info: JSON.stringify(req.body.info)
+    });
+    db.release();
+
+    res.status(200).json({ id: result.insertId });
+  }
+  catch (err) {
+    db.release();
+    res.status(400).json({ message: err });
+  }
 
 }
