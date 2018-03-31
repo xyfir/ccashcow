cde2f019ceb6794f08957772e3f7e5c8e38e8a3b
MrXyfir <mr@xyfir.com>
2018-03-30 18:20:35 -0700

    Integrate Admyn

---
+++ client/components/Admin.jsx
@@ -0,0 +1,11 @@
+import AdminPanel from 'admyn/client';
+import { render } from 'react-dom';
+import React from 'react';
+
+render(
+  <AdminPanel
+    title='xyPayments Admin'
+    api='/admyn-bmWjQ5O3q9RCoP60YPb1Xu8mCsUVTCv5pHbtVC13dPDN3Fhp2w8l/'
+  />,
+  document.getElementById('content')
+);
