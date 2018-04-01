125b0d1829e140239d1c3711228637de94e60a89
MrXyfir <mr@xyfir.com>
2018-04-01 11:42:20 -0700

    Update 'get payment' route/controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 /* PAYMENTS */
 router.post('/payments/initialize', require('./payments/initialize'));
 router.post('/payments/complete', require('./payments/complete'));
-router.get('/payments/status', require('./payments/status'));
+router.get('/payments/:payment', require('./payments/get'));
 
 module.exports = router;
