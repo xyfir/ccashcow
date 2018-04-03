d8bb94813b961ced99c520fa3ecb463486ddad78
MrXyfir <mr@xyfir.com>
2018-04-03 11:14:18 -0700

    Add `POST /api/payments/:payment/coinpayments/complete`

---
+++ server/controllers/payments/coinpayments/complete.js
@@ -0,0 +1,49 @@
+const getPayment = require('lib/payments/get');
+const CONFIG = require('constants/config');
+const MySQL = require('lib/MySQL');
+
+/**
+ * `POST /api/payments/:payment/coinpayments/complete`
+ * @param {object} req
+ * @param {object} req.params
+ * @param {number} req.params.payment
+ * @param {object} req.body
+ * @param {string} req.body.merchant
+ * @param {string} req.body.txn_id
+ * @param {string} req.body.currency
+ * @param {string} req.body.status
+ */
+/**
+ * @typedef {object} ResponseBody
+ * @prop {string} [message]
+ */
+module.exports = async function(req, res) {
+
+  const paymentId = req.params.payment;
+  const db = new MySQL;
+
+  try {
+    if (req.body.merchant != CONFIG.COINPAYMENTS.MERCHANT_ID)
+      throw 'Invalid request';
+    if (req.body.status < 1)
+      throw 'Payment not ready yet';
+
+    const payment = await getPayment(db, { paymentId, full: true });
+    db.release();
+
+    if (payment.paid !== null)
+      throw 'Payment has already been paid';
+
+    await db.query(
+      'UPDATE payments SET transaction = ?, method = ?, paid = NOW()',
+      [req.body.txn_id, `coinpayments:${req.body.currency.toLowerCase()}`]
+    );
+
+    res.status(200).json({ });
+  }
+  catch (err) {
+    db.release();
+    res.status(400).json({ message: err });
+  }
+
+}
