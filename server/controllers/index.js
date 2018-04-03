389852ff1a398247449128066b8e38cdb05d122b
MrXyfir <mr@xyfir.com>
2018-04-03 11:14:02 -0700

    Add CoinPayments routes/controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,5 +4,13 @@ const router = require('express').Router();
 router.post('/payments', require('./payments/initialize'));
 router.get('/payments/:payment', require('./payments/get'));
 router.post('/payments/:payment/square', require('./payments/square'));
+router.post(
+  '/payments/:payment/coinpayments',
+  require('./payments/coinpayments/create')
+);
+router.post(
+  '/payments/:payment/coinpayments/complete',
+  require('./payments/coinpayments/complete')
+);
 
 module.exports = router;
