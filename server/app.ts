513362ee0419af609e65e310e8cb3bf9a79731e4
MrXyfir <mr@xyfir.com>
2019-02-12 13:16:18 -0800

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,46 @@
-require('app-module-path').addPath(__dirname);
+import 'app-module-path/register';
+import { RICH_COW_WEB_URL, WEB_DIRECTORY, PORT, PROD } from 'constants/config';
+import * as bodyParser from 'body-parser';
+import * as Express from 'express';
+import { resolve } from 'path';
+import { router } from 'api/router';
 
-const express = require('express');
-const config = require('constants/config');
-const parser = require('body-parser');
-
-const app = express();
-
-/* Body Parser */
-app.use(parser.json({ limit: '5mb' }));
-app.use(parser.urlencoded({ extended: true, limit: '5mb' }));
-
-/* Routes / Controllers */
-app.use('/static', express.static(__dirname + '/static'));
-app.get('/pay', (req, res) => res.sendFile(__dirname + '/views/Pay.html'));
-app.use('/api', require('./controllers/'));
-
-/* Start Server */
-const server = app.listen(config.PORT, () =>
-  console.log('~~Server running on port', config.PORT)
+const app = Express();
+if (!PROD) {
+  // Needed to allow communication from webpack-dev-server host
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', RICH_COW_WEB_URL);
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+}
+app.use('/static', Express.static(resolve(WEB_DIRECTORY, 'dist')));
+app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use('/api', router);
+app.use(
+  (
+    err: string | Error,
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) => {
+    if (typeof err == 'string') {
+      res.status(400).json({ error: err });
+    } else {
+      console.error(err.stack);
+      res.status(500).json({ error: 'Something went wrong...' });
+    }
+  }
 );
-server.timeout = 300000;
-
-if (config.RUN_CRON_JOBS) require('./jobs/cron/start')();
+app.get('/*', (req, res) =>
+  res.sendFile(resolve(WEB_DIRECTORY, 'dist', 'index.html'))
+);
+app.listen(PORT, () => console.log('Listening on', PORT));
