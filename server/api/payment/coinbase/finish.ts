ba040941c0e4eb31c867ec01471cae2841df530c
MrXyfir <mr@xyfir.com>
2019-02-13 14:30:18 -0800

    Add finishCoinbasePayment()

--- server/api/payment/coinbase/finish.ts
+++ server/api/payment/coinbase/finish.ts
@@ -1,45 +1,12 @@
-const getPayment = require('lib/payments/get');
-const CONFIG = require('constants/config');
-const MySQL = require('lib/MySQL');
+import { NextFunction, Response, Request } from 'express';
+import { finishCoinbasePayment } from 'lib/payment/coinbase/finish';
 
-/**
- * `POST /api/payments/null/coinbase/complete`
- * @param {object} req
- * @param {object} req.body
- * @param {object} req.body.event
- * @param {string} req.body.event.id
- * @param {object} req.body.event.data
- * @param {object} req.body.event.data.metadata
- * @param {string} req.body.event.data.metadata.key
- * @param {number} req.body.event.data.metadata.paymentId
- */
-/**
- * @typedef {object} ResponseBody
- * @prop {string} [message]
- */
-export async function api_finishCoinbasePayment(req, res) {
-  const { paymentId } = req.body.event.data.metadata;
-  const db = new MySQL();
-
-  try {
-    if (req.body.event.data.metadata.key != CONFIG.COINBASE.WEBHOOK_SECRET)
-      throw 'Invalid request';
-
-    const payment = await getPayment(db, { paymentId, full: true });
-    if (payment.paid !== null) throw 'Payment has already been paid';
-
-    await db.query(
-      `
-      UPDATE payments SET transaction = ?, method = ?, paid = NOW()
-      WHERE id = ?
-    `,
-      [req.body.event.id, 'coinbase', paymentId]
-    );
-    db.release();
-
-    res.status(200).json({});
-  } catch (err) {
-    db.release();
-    res.status(400).json({ message: err });
-  }
+export function api_finishCoinbasePayment(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  finishCoinbasePayment(req.body.data.metadata.jwt, req.body.data.code)
+    .then(() => res.status(200).json({}))
+    .catch(next);
 }
---
+++ server/lib/payment/coinbase/finish.ts
@@ -0,0 +1,22 @@
+import { COINBASE_WEBHOOK_SECRET, STORAGE } from 'constants/config';
+import { verifyJWT } from 'lib/jwt/verify';
+import * as storage from 'node-persist';
+import { RichCow } from 'types/rich-cow';
+
+export async function finishCoinbasePayment(
+  jwt: string,
+  code: RichCow.Payment['coinbaseCommerceChargeCode']
+): Promise<void> {
+  const provided = await verifyJWT(jwt, COINBASE_WEBHOOK_SECRET);
+  await storage.init(STORAGE);
+  const payment: RichCow.Payment = await storage.getItem(
+    `payment-${provided.id}`
+  );
+  if (payment.paid) throw 'Payment has already been paid';
+
+  // Save completed charge
+  payment.paid = Date.now();
+  payment.method = 'coinbase-commerce';
+  payment.coinbaseCommerceChargeCode = code;
+  await storage.setItem(`payment-${payment.id}`, payment);
+}
