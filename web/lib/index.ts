25249a851a310248499b1ef78d7ce7abe013bc48
MrXyfir <mr@xyfir.com>
2019-02-13 19:31:10 -0800

    Add lib/index

---
+++ web/lib/index.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { App } from 'components/App';
+import 'typeface-roboto';
+
+render(React.createElement(App), document.getElementById('content'));
