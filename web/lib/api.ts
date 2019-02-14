e1ecf27b6fc0d9db1f52ba77a9702ebadbb63357
MrXyfir <mr@xyfir.com>
2019-02-14 12:43:39 -0800

    Remove withCredentials

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,7 +1,4 @@
 import { RICH_COW_API_URL } from 'constants/config';
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: RICH_COW_API_URL,
-  withCredentials: true
-});
+export const api = axios.create({ baseURL: RICH_COW_API_URL });
