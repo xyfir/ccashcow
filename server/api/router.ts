62176d7704f7964be4535bb66331de4353f306ee
MrXyfir <mr@xyfir.com>
2019-02-15 11:05:45 -0800

    Fix routes

--- server/api/router.ts
+++ server/api/router.ts
@@ -6,5 +6,11 @@ export const router = Router();
 router.get('/payment', c.api_getPayment);
 router.post('/payment/square/start', c.api_startSquarePayment);
 router.post('/payment/square/finish', c.api_finishSquarePayment);
-router.post('/payment/coinbase/start', c.api_startCoinbaseCommercePayment);
-router.post('/payment/coinbase/finish', c.api_finishCoinbaseCommercePayment);
+router.post(
+  '/payment/coinbase-commerce/start',
+  c.api_startCoinbaseCommercePayment
+);
+router.post(
+  '/payment/coinbase-commerce/finish',
+  c.api_finishCoinbaseCommercePayment
+);
