2d69e4102313d51fb74e634a6c3a11de5d407490
MrXyfir <mr@xyfir.com>
2019-06-16 14:15:17 -0700

    Upgrade to Material-UI v4

--- web/components/App.tsx
+++ web/components/App.tsx
@@ -1,7 +1,8 @@
-import { MuiThemeProvider, CssBaseline, Button } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
-import { Payment } from 'components/Payment';
+import { ThemeProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
 import { CCashCow } from 'types/ccashcow';
+import { Payment } from 'components/Payment';
 import * as React from 'react';
 import { theme } from 'lib/theme';
 import { hot } from 'react-hot-loader';
@@ -15,7 +16,7 @@ declare global {
 }
 
 const _App = () => (
-  <MuiThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <SnackbarProvider
       maxSnack={2}
@@ -23,7 +24,7 @@ const _App = () => (
     >
       <Payment />
     </SnackbarProvider>
-  </MuiThemeProvider>
+  </ThemeProvider>
 );
 
 export const App = hot(module)(_App);
